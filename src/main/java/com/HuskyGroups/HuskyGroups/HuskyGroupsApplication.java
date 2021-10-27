943f90cfe8a9b02a8adcd4abea45061692100531
xicn <43724191+xicn@users.noreply.github.com>
2021-10-27 12:44:44 -0400

    Initial commit

---
+++ src/main/java/com/HuskyGroups/HuskyGroups/HuskyGroupsApplication.java
@@ -0,0 +1,13 @@
+package com.HuskyGroups.HuskyGroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyGroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskyGroupsApplication.class, args);
+	}
+
+}
