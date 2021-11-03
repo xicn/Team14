8230ea9c9ecb01b86c83344401196c20712359cb
xicn <43724191+xicn@users.noreply.github.com>
2021-11-03 19:13:37 -0400

    Nov 3

---
+++ src/main/java/com/HuskyGroups/HuskyGroups/HomeController.java
@@ -0,0 +1,17 @@
+package com.HuskyGroups.HuskyGroups;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "welcome home!";
+    }
+}
